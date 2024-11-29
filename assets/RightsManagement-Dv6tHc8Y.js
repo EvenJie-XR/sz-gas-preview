32369963baf564ea5c008555cdfd2a816e3e15ee
zwj <1927773824@qq.com>
2024-11-30 00:16:09 +0800

    feat: 完成故障维修和预警设置页面

---
+++ assets/RightsManagement-Dv6tHc8Y.js
@@ -0,0 +1,1 @@
+import{aI as e,c as t,b as c}from"./index-BBqEydEq.js";const n={},s={class:"rights-management"};function a(o,r){return c(),t("div",s)}const i=e(n,[["render",a]]);export{i as default};
